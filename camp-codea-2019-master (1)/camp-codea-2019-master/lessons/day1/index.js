4ddca7439b13ba69790d2cd59a75fba9ebb46a59
KarenPhua <49655103+KarenPhua@users.noreply.github.com>
2019-08-09 15:41:13 -0400

    Update index.js

--- camp-codea-2019-master (1)/camp-codea-2019-master/lessons/day1/index.js
+++ camp-codea-2019-master (1)/camp-codea-2019-master/lessons/day1/index.js
@@ -1,2 +1,8 @@
 function cellClick(args){
+    const cell = args.target;
+    var currentText = cell.innerText
+    if(currentText == 'X')
+    cell.innerText = 'O';
+    else
+        cell.innerText = 'X';
 }
